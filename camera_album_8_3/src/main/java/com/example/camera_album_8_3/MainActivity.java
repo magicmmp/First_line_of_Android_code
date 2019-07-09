8da17db780861d9582a1869d1700c2c575f6d67f
magicmmp <2233973404@qq.com>
2019-07-09 22:34:18 +0800

    添加了选择相册照片的功能

--- camera_album_8_3/src/main/java/com/example/camera_album_8_3/MainActivity.java
+++ camera_album_8_3/src/main/java/com/example/camera_album_8_3/MainActivity.java
@@ -33,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
     private ImageView picture;
     private Uri imageUri;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
