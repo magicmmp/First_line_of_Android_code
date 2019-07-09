cf88f0527efa854d0737d2c153c68598d6436cb3
magicmmp <2233973404@qq.com>
2019-07-09 11:00:31 +0800

    Git项目提交测试

--- database_test_6_2/src/main/java/com/example/filesavedandread_6_2/MainActivity.java
+++ database_test_6_2/src/main/java/com/example/filesavedandread_6_2/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     private MyDatabaseHelper dbHelper;
 
     @Override
-    //Git测试，第一行代码
+    //Git测试，第一
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
